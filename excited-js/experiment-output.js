dcc44a1cf922003889f1970a4cac46c9e3244954
ajith-om <ajith_om@hotmail.com>
2021-01-28 10:41:53 +0530

    initial commit

---
+++ excited-js/experiment-output.js
@@ -0,0 +1,62 @@
+true
+
+false
+
+!true
+
+!!true
+
+!!(true)
+
+!!([])
+
+!!({})
+
+"" == false
+
+null == false
+
+Number(true)
+
++true
+
+true + true
+
+!!("0")
+
+!!(0 && {})
+
+null == undefined
+
+"im a string" instanceof String
+
+typeof "im a string"
+
+typeof String('a') ==  'string'
+
+String('a') == 'a'
+
+new String('a') == 'a';
+
+typeof new String('a');
+
+"foo" + + "abc";
+
+[] + {};
+
+{} + [];
+
+[] + [];
+
+{} + {};
+
+
+
+
+
+
+
+
+
+
+
